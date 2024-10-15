7ced790139ce92bbced6cad30e65001488da9228
venkatesh2344 <100852059+venkatesh2344@users.noreply.github.com>
2024-10-15 21:27:34 +0530

    added age filter (#1)

--- src/main/java/com/example/demo/StudentRepository.java
+++ src/main/java/com/example/demo/StudentRepository.java
@@ -5,10 +5,7 @@ package com.example.demo;
 
 import com.example.demo.Model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.awt.*;
-import java.io.IOException;
 import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student, Integer> {
@@ -16,4 +13,5 @@ public interface StudentRepository extends JpaRepository<Student, Integer> {
 
     List<Integer> getIdsByName(String name);
 
+    List<Student> findByAge(int age);
 }
